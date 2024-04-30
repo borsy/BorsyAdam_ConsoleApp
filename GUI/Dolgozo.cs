709f8f4966806943b4c8c55dca287317b9b043e2
Bereg Szgep <bereg@bereg.hu>
2024-04-30 18:28:41 +0200

    ...

---
+++ GUI/Dolgozo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    class Dolgozo
+    {
+        //"nev","neme","reszleg","belepesev","ber"
+
+        public string nev;
+        public string neme;
+        public string reszleg;
+        public int belepesev;
+        public int ber;
+
+        public Dolgozo(string nev, string neme, string reszleg, int belepesev, int ber)
+        {
+            this.nev = nev;
+            this.neme = neme;
+            this.reszleg = reszleg;
+            this.belepesev = belepesev;
+            this.ber = ber;
+        }
+    }
