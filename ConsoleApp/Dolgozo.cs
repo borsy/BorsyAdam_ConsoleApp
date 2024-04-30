bb61d874ca2b56b42ec15276c3a6d5747d415e17
Bereg Szgep <bereg@bereg.hu>
2024-04-30 17:08:28 +0200

    ...

---
+++ ConsoleApp/Dolgozo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp
+{
+    class Dolgozo
+    {
+        //"nev","neme","reszleg","belepesev","ber"
+
+        private string nev;
+        private string neme;
+        private string reszleg;
+        private int belepesev;
+        private int ber;
+
+        public Dolgozo(string nev, string neme, string reszleg, int belepesev, int ber)
+        {
+            this.nev = nev;
+            this.neme = neme;
+            this.reszleg = reszleg;
+            this.belepesev = belepesev;
+            this.ber = ber;
+        }
+
+
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp
 {
     class Program
     {
+
+        static List<Dolgozo> dolgozok = new List<Dolgozo>();
+
         static void Main(string[] args)
         {
+            beolvasas();
+
+            Console.WriteLine("Program vége!");
+            Console.ReadLine();
+        }
+
+        private static void beolvasas()
+        {
+            using (StreamReader sr = new StreamReader("dolgozok.csv"))
+            {
+                sr.ReadLine(); // --fejléc kihagyása, nem kell
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Replace('"', ' ').Split(',');
+                    Dolgozo dolgozo = new Dolgozo(line[0].Trim(), line[1].Trim(), line[2].Trim(), int.Parse(line[3].Trim()), int.Parse(line[4].Trim()));
+                    dolgozok.Add(dolgozo);
+                }
+            }
         }
     }
 }
