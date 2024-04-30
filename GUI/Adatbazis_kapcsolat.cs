57bf4d77f4da9e588b6757883eec00747f4a461a
Bereg Szgep <bereg@bereg.hu>
2024-04-30 18:45:59 +0200

    ...

---
+++ GUI/Adatbazis_kapcsolat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace GUI
+{
+    class Adatbazis_kapcsolat
+    {
+        MySqlCommand sqlparancs = null;
+        MySqlConnection adatbazis_kapcsolodas = null;
+
+        public Adatbazis_kapcsolat()
+        {
+            MySqlConnectionStringBuilder adatbazis_kapcsolat_felepitese = new MySqlConnectionStringBuilder();
+
+            adatbazis_kapcsolat_felepitese.Server = "localhost";
+            adatbazis_kapcsolat_felepitese.UserID = "root";
+            adatbazis_kapcsolat_felepitese.Password = "";
+            adatbazis_kapcsolat_felepitese.Database = "munkasok";
+            adatbazis_kapcsolodas = new MySqlConnection(adatbazis_kapcsolat_felepitese.ConnectionString);
+
+            try
+            {
+                adatbazis_kapcsolat_nyitasa();
+                sqlparancs = adatbazis_kapcsolodas.CreateCommand();
+                adatbazis_kapcsolat_zarasa();
+
+            }
+            catch (MySqlException adatbazis_kivitel_kezeles)
+            {
+                MessageBox.Show(adatbazis_kivitel_kezeles.Message);
+                Environment.Exit(0);
+             
+            }
+        }
+
+        private void adatbazis_kapcsolat_zarasa()
+        {
+            if (adatbazis_kapcsolodas.State == System.Data.ConnectionState.Open)
+            {
+                adatbazis_kapcsolodas.Close();
+            }
+        }
+
+        private void adatbazis_kapcsolat_nyitasa()
+        {
+            if (adatbazis_kapcsolodas.State== System.Data.ConnectionState.Open)
+            {
+                adatbazis_kapcsolodas.Open();
+            }
+        }
+    }
+}
